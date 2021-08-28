f9673840a211c88338e5034cb311ff5281134335
Yonathan Uchoa S. Kaveski <yonathan.uchoa@gmail.com>
2021-08-28 18:15:08 -0400

    Configuracoes iniciais
    Configuracoes Iniciais do gradle.

---
+++ src/main/java/com/kaveski/yonathan/Controle_de_Ponto/ControleDePontoApplication.java
@@ -0,0 +1,13 @@
+package com.kaveski.yonathan.Controle_de_Ponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDePontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDePontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaveski/yonathan/Controle_de_Ponto/ControleDePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaveski.yonathan.Controle_de_Ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
